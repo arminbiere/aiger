dad0c1e7445c78449ebb107a8201553255e46d75
biere <biere>
2006-11-20 17:07:29 +0000

    fixed

--- aigsim.c
+++ aigsim.c
@@ -47,7 +47,15 @@ die (const char * fmt, ...)
 static unsigned char
 deref (unsigned lit)
 {
-  return val[aiger_lit2var(lit)] ^ aiger_sign(lit);
+  unsigned res = val[aiger_lit2var(lit)];
+  res ^= aiger_sign(lit);
+#ifndef NDEBUG
+  if (lit == 0)
+    assert (res == 0);
+  if (lit == 1)
+    assert (res == 1);
+#endif
+  return res;
 }
 
 static void
@@ -80,7 +88,7 @@ aiger_symbol_as_string (aiger_symbol * s)
 }
 
 #define USAGE \
-"usage: aigsim [-h][-c][-r n] <model> [<stimulus>]\n" \
+"usage: aigsim [-h][-c][-r n] [ <model> [<stimulus>] ]\n" \
 "\n" \
 "-h              usage\n" \
 "-c              check for witness and do not print trace\n" \
@@ -90,7 +98,7 @@ aiger_symbol_as_string (aiger_symbol * s)
 "-3              enable three valued stimulus in random simulation\n" \
 "-r <vectors>    random stimulus of <vectors> input vectors\n" \
 "-s <seed>       set seed of random number generator (default '0')\n" \
-"<model>         as AIG in AIGER format\n" \
+"<model>         AIG in AIGER format\n" \
 "<stimulus>      stimulus (file of 0/1/x input vectors)\n"
 
 int
@@ -99,7 +107,7 @@ main (int argc, char **argv)
   int vectors, check, vcd, found, print, quit, three, ground, seeded;
   const char *stimulus_file_name, *model_file_name, *error;
   unsigned i, j, s, l, r, tmp, seed;
-  int res, ch;
+  int ch;
 
   stimulus_file_name = model_file_name = 0;
   seeded = vcd = quit = check = 0;
@@ -149,8 +157,8 @@ main (int argc, char **argv)
 	die ("more than two files specified");
     }
 
-  if (!model_file_name)
-    die ("no model specified");
+  if (!model_file_name && vectors < 0)
+    die ("can only read model from <stdin> in random simulation mode");
 
   if (vectors >= 0 && stimulus_file_name)
     die ("random simulation but also stimulus file specified");
@@ -167,230 +175,205 @@ main (int argc, char **argv)
   if (check && vcd)
     die ("can not combine '-v' with '-c'");
 
-  res = 0;
-
   model = aiger_init ();
-  error = aiger_open_and_read_from_file (model, model_file_name);
+
+  if (model_file_name)
+    error = aiger_open_and_read_from_file (model, model_file_name);
+  else
+    error = aiger_read_from_file (model, stdin);
 
   if (error)
+    die ("%s", error);
+
+  aiger_reencode (model);	/* otherwise simulation incorrect */
+
+  if (stimulus_file_name)
     {
-      fprintf (stderr, "*** [aigsim] %s\n", error);
-      res = 1;
+      file = fopen (stimulus_file_name, "r");
+      if (!file)
+	die ("failed to open '%s'", stimulus_file_name);
+
+      close_file = 1;
     }
   else
+    file = stdin;
+
+  if (vcd)
     {
-      if (stimulus_file_name)
+      for (i = 0; i < model->num_inputs; i++)
+	printf ("$var wire 1 %s %s $end\n",
+		idx_as_vcd_id ('i', i),
+		aiger_symbol_as_string (model->inputs + i));
+
+      for (i = 0; i < model->num_latches; i++)
+	printf ("$var reg 1 %s %s $end\n",
+		idx_as_vcd_id ('l', i),
+		aiger_symbol_as_string (model->latches + i));
+
+      for (i = 0; i < model->num_outputs; i++)
+	printf ("$var wire 1 %s %s $end\n",
+		idx_as_vcd_id ('o', i),
+		aiger_symbol_as_string (model->outputs + i));
+
+      printf ("$enddefinitions $end\n");
+    }
+
+  val = calloc (model->maxvar + 1, sizeof (val[0]));
+
+  if (seeded)
+    srand (seed);
+
+  i = 1;
+
+  while (vectors)
+    {
+      if (vectors > 0)
 	{
-	  file = fopen (stimulus_file_name, "r");
-	  if (!file)
+	  for (j = 1; j <= model->num_inputs; j++)
 	    {
-	      fprintf (stderr,
-		       "*** [aigsim] failed to open '%s'\n",
-		       stimulus_file_name);
-	      res = 1;
+	      s = 17 * j + i;
+	      s %= 20;
+	      tmp = rand() >> s;
+	      tmp %= three + 2;
+	      val [j] = tmp;
 	    }
-	  else
-	    close_file = 1;
+
+	  vectors--;
 	}
       else
-	file = stdin;
-
-      if (!res)
 	{
-	  if (vcd)
+	  j = 1;
+	  ch = getc (file);
+
+	  if (ch == EOF)
+	    break;
+
+	  /* First read and overwrite inputs.
+	   */
+	  while (j <= model->num_inputs)
 	    {
-	      for (i = 0; i < model->num_inputs; i++)
-		printf ("$var wire 1 %s %s $end\n",
-		        idx_as_vcd_id ('i', i),
-			aiger_symbol_as_string (model->inputs + i));
-
-	      for (i = 0; i < model->num_latches; i++)
-		printf ("$var reg 1 %s %s $end\n",
-		        idx_as_vcd_id ('l', i),
-			aiger_symbol_as_string (model->latches + i));
-
-	      for (i = 0; i < model->num_outputs; i++)
-		printf ("$var wire 1 %s %s $end\n",
-		        idx_as_vcd_id ('o', i),
-			aiger_symbol_as_string (model->outputs + i));
-
-	      printf ("$enddefinitions $end\n");
-	    }
-
-	  val = calloc (model->maxvar + 1, sizeof (val[0]));
-
-	  if (seeded)
-	    srand (seed);
-
-	  i = 1;
-
-	  while (!res && vectors)
-	    {
-	      if (vectors > 0)
-		{
-		  for (j = 1; j <= model->num_inputs; j++)
-		    {
-		      s = 17 * j + i;
-		      s %= 20;
-		      tmp = rand() >> s;
-		      tmp %= three + 2;
-		      val [j] = tmp;
-		    }
-
-		  vectors--;
-		}
+	      if (ch == '0')
+		val[j] = 0;
+	      else if (ch == '1')
+		val[j] = 1;
+	      else if (ch == 'x')
+		val[j] = ground ? 0 : 2;
 	      else
-		{
-		  j = 1;
-		  ch = getc (file);
+		die ("line %u: pos %u: expected '0' or '1'", i, j);
 
-		  if (ch == EOF)
-		    break;
-
-		  /* First read and overwrite inputs.
-		   */
-		  while (j <= model->num_inputs)
-		    {
-		      if (ch == '0')
-			val[j] = 0;
-		      else if (ch == '1')
-			val[j] = 1;
-		      else if (ch == 'x')
-			val[j] = ground ? 0 : 2;
-		      else
-			{
-			  fprintf (stderr,
-				   "*** [aigsim] "
-				   "line %u: pos %u: expected '0' or '1'\n",
-				   i, j);
-			  res = 1;
-			  break;
-			}
-
-		      j++;
-		      ch = getc (file);
-		    }
-
-		  if (res)
-		    break;
-
-		  if (ch != '\n')
-		    {
-		      fprintf (stderr,
-			       "*** [aigsim] "
-			       "line %u: pos %u: expected new line\n", i, j);
-		      res = 1;
-		      break;
-		    }
-		}
-
-	      /* Simulate AND nodes.
-	       */
-	      for (j = 0; j < model->num_ands; j++)
-		{
-		  aiger_and *and = model->ands + j;
-		  l = deref (and->rhs0);
-		  r = deref (and->rhs1);
-		  tmp = l & r;
-		  tmp |= l & (r << 1);
-		  tmp |= r & (l << 1);
-		  val[and->lhs / 2] = tmp;
-		}
-
-	      found = 0;
-	      for (j = 0; !found && j < model->num_outputs; j++)
-		found = (deref (model->outputs[j].lit) == 1);
-
-	      print = !vcd && (!check || found);
-
-	      /* Print current state of latches.
-	       */
-	      if (print)
-		{
-		  for (j = 0; j < model->num_latches; j++)
-		    put (model->latches[j].lit);
-		  fputc (' ', stdout);
-		}
-
-	      if (vcd)
-		{
-		  printf ("#%u\n", i - 1);
-
-		  if (i == 1)
-		    printf ("$dumpvars\n");
-
-		  for (j = 0; j < model->num_latches; j++)
-		    {
-		      put (model->latches[j].lit);
-		      fputs (idx_as_vcd_id ('l', j), stdout);
-		      fputc ('\n', stdout);
-		    }
-
-		  if (i == 1)
-		    printf ("$end\n");
-		}
-
-	      /* Then update latches.
-	       */
-	      for (j = 0; j < model->num_latches; j++)
-		{
-		  aiger_symbol *symbol = model->latches + j;
-		  val[symbol->lit / 2] = deref (symbol->next);
-		}
-
-	      if (print)
-		{
-		  /* Print inputs.
-		   */
-		  for (j = 0; j < model->num_inputs; j++)
-		    put (model->inputs[j].lit);
-		  fputc (' ', stdout);
-
-		  /* Print outputs.
-		   */
-		  for (j = 0; j < model->num_outputs; j++)
-		    put (model->outputs[j].lit);
-		  fputc (' ', stdout);
-
-		  /* Print next state of latches.
-		   */
-		  for (j = 0; j < model->num_latches; j++)
-		    put (model->latches[j].lit);
-
-		  fputc ('\n', stdout);
-		}
-
-	      if (vcd)
-		{
-		  for (j = 0; j < model->num_inputs; j++)
-		    {
-		      put (model->inputs[j].lit);
-		      fputs (idx_as_vcd_id ('i', j), stdout);
-		      fputc ('\n', stdout);
-		    }
-
-		  for (j = 0; j < model->num_outputs; j++)
-		    {
-		      put (model->outputs[j].lit);
-		      fputs (idx_as_vcd_id ('o', j), stdout);
-		      fputc ('\n', stdout);
-		    }
-		}
-
-	      i++;
-
-	      if (found && quit)
-		break;
+	      j++;
+	      ch = getc (file);
 	    }
 
-	  free (val);
+	  if (ch != '\n')
+	    die ("line %u: pos %u: expected new line", i, j);
 	}
 
-      if (close_file)
-	fclose (file);
+      /* Simulate AND nodes.
+       */
+      for (j = 0; j < model->num_ands; j++)
+	{
+	  aiger_and *and = model->ands + j;
+	  l = deref (and->rhs0);
+	  r = deref (and->rhs1);
+	  tmp = l & r;
+	  tmp |= l & (r << 1);
+	  tmp |= r & (l << 1);
+	  val[and->lhs / 2] = tmp;
+	}
+
+      found = 0;
+      for (j = 0; !found && j < model->num_outputs; j++)
+	found = (deref (model->outputs[j].lit) == 1);
+
+      print = !vcd && (!check || found);
+
+      /* Print current state of latches.
+       */
+      if (print)
+	{
+	  for (j = 0; j < model->num_latches; j++)
+	    put (model->latches[j].lit);
+	  fputc (' ', stdout);
+	}
+
+      if (vcd)
+	{
+	  printf ("#%u\n", i - 1);
+
+	  if (i == 1)
+	    printf ("$dumpvars\n");
+
+	  for (j = 0; j < model->num_latches; j++)
+	    {
+	      put (model->latches[j].lit);
+	      fputs (idx_as_vcd_id ('l', j), stdout);
+	      fputc ('\n', stdout);
+	    }
+
+	  if (i == 1)
+	    printf ("$end\n");
+	}
+
+      /* Then update latches.
+       */
+      for (j = 0; j < model->num_latches; j++)
+	{
+	  aiger_symbol *symbol = model->latches + j;
+	  val[symbol->lit / 2] = deref (symbol->next);
+	}
+
+      if (print)
+	{
+	  /* Print inputs.
+	   */
+	  for (j = 0; j < model->num_inputs; j++)
+	    put (model->inputs[j].lit);
+	  fputc (' ', stdout);
+
+	  /* Print outputs.
+	   */
+	  for (j = 0; j < model->num_outputs; j++)
+	    put (model->outputs[j].lit);
+	  fputc (' ', stdout);
+
+	  /* Print next state of latches.
+	   */
+	  for (j = 0; j < model->num_latches; j++)
+	    put (model->latches[j].lit);
+
+	  fputc ('\n', stdout);
+	}
+
+      if (vcd)
+	{
+	  for (j = 0; j < model->num_inputs; j++)
+	    {
+	      put (model->inputs[j].lit);
+	      fputs (idx_as_vcd_id ('i', j), stdout);
+	      fputc ('\n', stdout);
+	    }
+
+	  for (j = 0; j < model->num_outputs; j++)
+	    {
+	      put (model->outputs[j].lit);
+	      fputs (idx_as_vcd_id ('o', j), stdout);
+	      fputc ('\n', stdout);
+	    }
+	}
+
+      i++;
+
+      if (found && quit)
+	break;
     }
+
+  free (val);
+
+  if (close_file)
+    fclose (file);
 
   aiger_reset (model);
 
-  return res;
+  return 0;
 }
