29bfddaee8a5fd5ba851a333822001bc01dbac9b
biere <biere>
2006-12-15 20:58:18 +0000

    alpha

--- aigtodot.c
+++ aigtodot.c
@@ -18,13 +18,13 @@ die (const char * fmt, ...)
   exit (1);
 }
 
-static aiger * model;
-
 int
 main (int argc, char ** argv)
 {
-  const char * model_name, * dot_name;
-  int i;
+  const char * model_name, * dot_name, * err;
+  int i, close_dot_file;
+  FILE * dot_file;
+  aiger * model;
 
   model_name = dot_name = 0;
 
@@ -49,7 +49,49 @@ main (int argc, char ** argv)
     }
 
   model = aiger_init ();
+  if (model_name)
+    err = aiger_open_and_read_from_file (model, model_name);
+  else
+    err = aiger_read_from_file (model, stdin);
+
+  if (err)
+    die ("%s", err);
+
+  if (dot_name)
+    {
+      dot_file = fopen (dot_name, "w");
+      if (!dot_file)
+	die ("can not write to '%s'", dot_name);
+      close_dot_file = 1;
+    }
+  else
+    {
+      dot_file = stdout;
+      close_dot_file = 0;
+    }
+
+  fputs ("digraph {\n", dot_file);
+
+  for (i = 0; i < model->num_ands; i++)
+    {
+      aiger_and * and = model->ands + i;
+
+      fprintf (dot_file, "\"%u\"->\"%u\"[arrowhead=", and->lhs/2, and->rhs0/2);
+      fputs (((and->rhs0 & 1) ? "dot" : "none"), dot_file);
+      fputs ("];\n", dot_file);
+
+      fprintf (dot_file, "\"%u\"->\"%u\"[arrowhead=", and->lhs/2, and->rhs1/2);
+      fputs (((and->rhs1 & 1) ? "dot" : "none"), dot_file);
+      fputs ("];\n", dot_file);
+    }
+
+  fputs ("}\n", dot_file);
+
+  if (close_dot_file)
+    fclose (dot_file);
+
   aiger_reset (model);
+
 
   return 0;
 }
