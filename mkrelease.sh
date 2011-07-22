fd027b84f0e76b2b954573a54dd84401b58d1b3d
Armin Biere <biere@jku.at>
2011-07-22 15:22:17 +0200

    added aigbmc

--- mkrelease.sh
+++ mkrelease.sh
@@ -1,2 +1,25 @@
 #!/bin/sh
 version=`cat VERSION`
+name=aiger-$version
+archive=/tmp/${name}.tar.gz
+dir=/tmp/$name
+rm -rf $dir
+mkdir $dir
+make -C doc/beyond1 2>/dev/null >/dev/null
+cp -a configure makefile.in $dir
+cp -a VERSION README FORMAT LICENSE $dir
+cp -a doc/beyond1/beyond1.pdf $dir
+cp -a \
+aigand.c aigbmc.c aigdd.c aiger.c aiger.h aigfuzz.c aigfuzz.h \
+aigfuzzlayers.c aiginfo.c aigjoin.c aigmiter.c aigmove.c aignm.c aigor.c \
+aigsim.c aigsplit.c aigstrip.c aigtoaig.c aigtoblif.c aigtocnf.c \
+aigtodot.c aigtosmv.c aigunroll.c andtoaig.c bliftoaig.c simpaig.c \
+simpaig.h smvtoaig.c soltostim.c wrapstim.c \
+$dir
+cp -a mc.sh aigvis $dir
+cp -ar examples $dir/
+cd /tmp
+rm -f $archive
+tar cf $archive $name
+rm -rf $dir
+ls -l $archive
