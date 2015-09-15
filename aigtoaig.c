008f862b81fae26390c77b781acdb1c5c72d1f88
Armin Biere <biere@jku.at>
2015-09-15 14:31:53 +0200

    removing optionally outputs in aigtoaig

--- aigtoaig.c
+++ aigtoaig.c
@@ -1,5 +1,5 @@
 /***************************************************************************
-Copyright (c) 2006-2007, Armin Biere, Johannes Kepler University.
+Copyright (c) 2006-2015, Armin Biere, Johannes Kepler University.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to
@@ -122,6 +122,8 @@ die (const char *fmt, ...)
 "  src    input file or '-' for 'stdin'\n" \
 "  dst    output file or '-' for 'stdout'\n" \
 "\n" \
+"  --remove-outputs\n" \
+"\n" \
 "The input format is given by the header in the input file, while\n" \
 "the output format is determined by the name of the output file.\n" \
 "If the name of the output file has a '.aag' or '.aag.gz' suffix or '-a'\n" \
@@ -136,6 +138,8 @@ main (int argc, char **argv)
   const char *src, *dst, *src_name, *dst_name, *error;
   int verbose, ascii, strip, res;
   stream reader, writer;
+  unsigned num_outputs;
+  int remove_outputs;
   aiger_mode mode;
   memory memory;
   aiger *aiger;
@@ -143,6 +147,8 @@ main (int argc, char **argv)
 
   res = verbose = ascii = strip = 0;
   src_name = dst_name = src = dst = 0;
+  remove_outputs = 0;
+  num_outputs = 0;
 
   for (i = 1; i < argc; i++)
     {
@@ -157,6 +163,8 @@ main (int argc, char **argv)
 	strip = 1;
       else if (!strcmp (argv[i], "-a"))
 	ascii = 1;
+      else if (!strcmp (argv[i], "--remove-outputs"))
+	remove_outputs = 1;
       else if (argv[i][0] == '-' && argv[i][1])
 	die ("invalid command line option '%s'", argv[i]);
       else if (!src_name)
@@ -250,6 +258,19 @@ main (int argc, char **argv)
 	    }
 	}
 
+      if (remove_outputs)
+	{
+	  if (verbose)
+	    {
+	      fprintf (stderr,
+		       "[aigtoaig] removing %u outputs\n",
+		       aiger->num_outputs);
+	      fflush (stderr);
+	  }
+	  num_outputs = aiger->num_outputs;
+	  aiger->num_outputs = 0;
+	}
+
       if (dst)
 	{
 	  if (aiger_open_and_write_to_file (aiger, dst))
@@ -295,6 +316,9 @@ main (int argc, char **argv)
 	    }
 	}
     }
+
+  if (remove_outputs)
+    aiger->num_outputs = num_outputs;
 
   aiger_reset (aiger);
 
