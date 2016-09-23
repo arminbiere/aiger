959b0fb6ca93da862a42be3821c0399d39b12c22
Armin Biere <biere@jku.at>
2016-09-23 09:33:55 +0200

    added aigtobtor

--- mkrelease.sh
+++ mkrelease.sh
@@ -12,9 +12,10 @@ cp -a doc/beyond1/beyond1.pdf $dir
 cp -a \
 aigand.c aigbmc.c aigdd.c aiger.c aiger.h aigfuzz.c aigflip.c aigfuzz.h \
 aigfuzzlayers.c aiginfo.c aigjoin.c aigmiter.c aigmove.c aignm.c aigor.c \
-aigreset.c aigsim.c aigsplit.c aigstrip.c aigtoaig.c aigtoblif.c aigtocnf.c \
-aigtodot.c aigtosmv.c aigunroll.c andtoaig.c bliftoaig.c simpaig.c \
-simpaig.h smvtoaig.c soltostim.c wrapstim.c aigunconstraint.c aigdep.c \
+aigreset.c aigsim.c aigsplit.c aigstrip.c aigtoaig.c aigtoblif.c \
+aigtobtor.c aigtocnf.c aigtodot.c aigtosmv.c aigunroll.c andtoaig.c \
+bliftoaig.c simpaig.c simpaig.h smvtoaig.c soltostim.c wrapstim.c \
+aigunconstraint.c aigdep.c \
 $dir
 cp -a mc.sh aigvis $dir
 cp -ar examples $dir/
