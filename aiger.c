6b22a414d3786df77189ffbd743ae86bd1411491
Armin Biere <biere@jku.at>
2018-11-09 04:47:15 +0100

    copyright

--- aiger.c
+++ aiger.c
@@ -1,6 +1,6 @@
 /***************************************************************************
 Copyright (c) 2011, Siert Wieringa, Aalto University, Finland.
-Copyright (c) 2006-2011, Armin Biere, Johannes Kepler University.
+Copyright (c) 2006-2018, Armin Biere, Johannes Kepler University.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to
--- aigfuzz.c
+++ aigfuzz.c
@@ -1,5 +1,5 @@
 /***************************************************************************
-Copyright (c) 2009-2011, Armin Biere, Johannes Kepler University.
+Copyright (c) 2009-2018, Armin Biere, Johannes Kepler University.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to
