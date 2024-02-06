f895bad3aba8dcfeebf979916e2bd37676f1a284
Armin Biere <biere@informatik.uni-freiburg.de>
2024-02-06 13:59:41 +0100

    added is_ite

--- aigtocnf.c
+++ aigtocnf.c
@@ -21,19 +21,18 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 IN THE SOFTWARE.
 ***************************************************************************/
 
-#include "aiger.h"
-
 #include <limits.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include "aiger.h"
+
 static int verbose;
 
 static void msg(const char *fmt, ...) {
   va_list ap;
-  if (!verbose)
-    return;
+  if (!verbose) return;
   fputs("[aigtocnf] ", stderr);
   va_start(ap, fmt);
   vfprintf(stderr, fmt, ap);
@@ -71,28 +70,66 @@ static int is_xor(aiger *aiger, unsigned lit, unsigned *rhs0ptr,
   unsigned right_rhs0, right_rhs1;
   unsigned not_right_rhs0, not_right_rhs1;
   and = aiger_is_and(aiger, lit);
-  if (!and)
-    return 0;
-  if (!aiger_sign(and->rhs0))
-    return 0;
-  if (!aiger_sign(and->rhs1))
-    return 0;
+  if (!and) return 0;
+  if (!aiger_sign(and->rhs0)) return 0;
+  if (!aiger_sign(and->rhs1)) return 0;
   left = aiger_is_and(aiger, and->rhs0);
-  if (!left)
-    return 0;
+  if (!left) return 0;
   right = aiger_is_and(aiger, and->rhs1);
-  if (!right)
-    return 0;
+  if (!right) return 0;
   left_rhs0 = left->rhs0, left_rhs1 = left->rhs1;
   right_rhs0 = right->rhs0, right_rhs1 = right->rhs1;
   not_right_rhs0 = aiger_not(right_rhs0);
   not_right_rhs1 = aiger_not(right_rhs1);
   if ((left_rhs0 == not_right_rhs0 && left_rhs1 == not_right_rhs1) ||
       (left_rhs0 == not_right_rhs1 && left_rhs1 == not_right_rhs0)) {
-    if (rhs0ptr)
-      *rhs0ptr = left_rhs0;
-    if (rhs1ptr)
-      *rhs1ptr = left_rhs1;
+    if (rhs0ptr) *rhs0ptr = left_rhs0;
+    if (rhs1ptr) *rhs1ptr = left_rhs1;
+    return 1;
+  }
+  return 0;
+}
+
+static int is_ite(aiger *aiger, unsigned lit, unsigned *cond_ptr,
+		  unsigned *then_lit_ptr, unsigned *else_lit_ptr) {
+  aiger_and *and, *left, *right;
+  unsigned left_rhs0, left_rhs1;
+  unsigned right_rhs0, right_rhs1;
+  unsigned not_left_rhs0, not_left_rhs1;
+  unsigned not_right_rhs0, not_right_rhs1;
+  and = aiger_is_and(aiger, lit);
+  if (!and) return 0;
+  if (!aiger_sign(and->rhs0)) return 0;
+  if (!aiger_sign(and->rhs1)) return 0;
+  left = aiger_is_and(aiger, and->rhs0);
+  if (!left) return 0;
+  right = aiger_is_and(aiger, and->rhs1);
+  if (!right) return 0;
+  left_rhs0 = left->rhs0, left_rhs1 = left->rhs1;
+  right_rhs0 = right->rhs0, right_rhs1 = right->rhs1;
+  not_left_rhs0 = aiger_not(left_rhs0);
+  not_left_rhs1 = aiger_not(left_rhs1);
+  not_right_rhs0 = aiger_not(right_rhs0);
+  not_right_rhs1 = aiger_not(right_rhs1);
+  if (left_rhs0 == not_right_rhs0) {
+    *cond_ptr = not_left_rhs0;
+    *then_lit_ptr = left_rhs1;
+    *else_lit_ptr = right_rhs1;
+    return 1;
+  } else if (left_rhs0 == not_right_rhs1) {
+    *cond_ptr = not_left_rhs0;
+    *then_lit_ptr = left_rhs0;
+    *else_lit_ptr = right_rhs0;
+    return 1;
+  } else if (left_rhs1 == not_right_rhs0) {
+    *cond_ptr = not_left_rhs0;
+    *then_lit_ptr = left_rhs0;
+    *else_lit_ptr = right_rhs1;
+    return 1;
+  } else if (left_rhs1 == not_right_rhs1) {
+    *cond_ptr = not_left_rhs0;
+    *then_lit_ptr = left_rhs0;
+    *else_lit_ptr = right_rhs0;
     return 1;
   }
   return 0;
@@ -113,9 +150,10 @@ int main(int argc, char **argv) {
 
   for (i = 1; i < argc; i++) {
     if (!strcmp(argv[i], "-h")) {
-      fprintf(stderr, "usage: "
-		      "aigtocnf [-h][-v][--no-pg][--no-xor][-m][<aig-file> "
-		      "[<dimacs-file>]]\n");
+      fprintf(stderr,
+	      "usage: "
+	      "aigtocnf [-h][-v][--no-pg][--no-xor][-m][<aig-file> "
+	      "[<dimacs-file>]]\n");
       exit(0);
     } else if (!strcmp(argv[i], "-m"))
       prtmap = 1;
@@ -142,32 +180,25 @@ int main(int argc, char **argv) {
   else
     error = aiger_read_from_file(aiger, stdin);
 
-  if (error)
-    die("%s: %s", input_name ? input_name : "<stdin>", error);
+  if (error) die("%s: %s", input_name ? input_name : "<stdin>", error);
 
   msg("read MILOA %u %u %u %u %u", aiger->maxvar, aiger->num_inputs,
       aiger->num_latches, aiger->num_outputs, aiger->num_ands);
 
-  if (aiger->num_latches)
-    die("can not handle latches");
+  if (aiger->num_latches) die("can not handle latches");
   if (aiger->num_bad)
     die("can not handle bad state properties (use 'aigmove')");
   if (aiger->num_constraints)
     die("can not handle environment constraints (use 'aigmove')");
-  if (!aiger->num_outputs)
-    die("no output");
-  if (aiger->num_outputs > 1)
-    die("more than one output");
-  if (aiger->num_justice)
-    wrn("ignoring justice properties");
-  if (aiger->num_fairness)
-    wrn("ignoring fairness constraints");
+  if (!aiger->num_outputs) die("no output");
+  if (aiger->num_outputs > 1) die("more than one output");
+  if (aiger->num_justice) wrn("ignoring justice properties");
+  if (aiger->num_fairness) wrn("ignoring fairness constraints");
 
   close_file = 0;
   if (output_name) {
     file = fopen(output_name, "w");
-    if (!file)
-      die("failed to write '%s'", output_name);
+    if (!file) die("failed to write '%s'", output_name);
     close_file = 1;
   } else
     file = stdout;
@@ -196,7 +227,7 @@ int main(int argc, char **argv) {
       lhs = and->lhs;
       rhs0 = and->rhs0;
       rhs1 = and->rhs1;
-      xor = !noxor && is_xor(aiger, lhs, &rhs0, &rhs1);
+      xor = !noxor &&is_xor(aiger, lhs, &rhs0, &rhs1);
       not_lhs = aiger_not(lhs);
       not_rhs0 = aiger_not(rhs0);
       not_rhs1 = aiger_not(rhs1);
@@ -213,10 +244,8 @@ int main(int argc, char **argv) {
     if (nopg) {
       for (lit = 2; lit != 2 * aiger->maxvar; lit += 2) {
 	unsigned not_lit = lit + 1;
-	if (refs[lit] && !refs[not_lit])
-	  refs[not_lit] = UINT_MAX;
-	if (!refs[lit] && refs[not_lit])
-	  refs[lit] = UINT_MAX;
+	if (refs[lit] && !refs[not_lit]) refs[not_lit] = UINT_MAX;
+	if (!refs[lit] && refs[not_lit]) refs[lit] = UINT_MAX;
       }
     }
 
@@ -232,32 +261,24 @@ int main(int argc, char **argv) {
     for (lit = 2; lit <= 2 * aiger->maxvar; lit += 2) {
       unsigned not_lit;
       not_lit = lit + 1;
-      if (!refs[lit] && !refs[not_lit])
-	continue;
+      if (!refs[lit] && !refs[not_lit]) continue;
       map[lit] = ++m;
       map[not_lit] = -m;
-      if (prtmap)
-	fprintf(file, "c %d -> %d\n", lit, m);
-      if (!aiger_is_and(aiger, lit))
-	continue;
+      if (prtmap) fprintf(file, "c %d -> %d\n", lit, m);
+      if (!aiger_is_and(aiger, lit)) continue;
       if (!noxor && is_xor(aiger, lit, 0, 0)) {
-	if (refs[lit])
-	  n += 2;
-	if (refs[not_lit])
-	  n += 2;
+	if (refs[lit]) n += 2;
+	if (refs[not_lit]) n += 2;
       } else {
-	if (refs[lit])
-	  n += 2;
-	if (refs[not_lit])
-	  n += 1;
+	if (refs[lit]) n += 2;
+	if (refs[not_lit]) n += 1;
       }
     }
 
     fprintf(file, "p cnf %u %u\n", m, n);
     msg("p cnf %u %u", m, n);
 
-    if (refs[0] || refs[1])
-      fprintf(file, "%d 0\n", map[1]);
+    if (refs[0] || refs[1]) fprintf(file, "%d 0\n", map[1]);
 
     for (i = 0; i < aiger->num_ands; i++) {
       unsigned not_lhs, not_rhs0, not_rhs1;
@@ -268,7 +289,7 @@ int main(int argc, char **argv) {
       lhs = and->lhs;
       rhs0 = and->rhs0;
       rhs1 = and->rhs1;
-      xor = !noxor && is_xor(aiger, lhs, &rhs0, &rhs1);
+      xor = !noxor &&is_xor(aiger, lhs, &rhs0, &rhs1);
       not_lhs = aiger_not(lhs);
       not_rhs0 = aiger_not(rhs0);
       not_rhs1 = aiger_not(rhs1);
@@ -297,8 +318,7 @@ int main(int argc, char **argv) {
     free(map);
   }
 
-  if (close_file)
-    fclose(file);
+  if (close_file) fclose(file);
   aiger_reset(aiger);
 
   return res;
